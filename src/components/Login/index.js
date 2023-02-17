78c88f7a23be0404539352482e2e26087bba7561
jagadeesh131 <jagadeeshkumarkadiri561@gamail.com>
2023-02-17 05:40:42 +0000

    Login App

---
+++ src/components/Login/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const LogIn = props => {
+  const {login} = props
+  return (
+    <button className="login-btn" type="button" onClick={login}>
+      Login
+    </button>
+  )
+}
+
+export default LogIn
